f88c1759e3487c3dbe575dfb091e43019c9697e2
TanishqBhardwaj <47564288+TanishqBhardwaj@users.noreply.github.com>
2021-04-13 23:02:23 +0530

    Initial Commit

---
+++ app/src/main/java/in/silive/bytepad/MainActivity.java
@@ -0,0 +1,14 @@
+package in.silive.bytepad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

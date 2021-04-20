c59f28eec3f9eb34cdb0557475791185d7257c4f
Administrator <garima1913076@akgec.ac.in>
2021-04-20 21:04:35 +0530

    Completed homescreen UI

--- app/src/main/java/in/silive/bytepad/Ui/Activities/MainActivity.java
+++ app/src/main/java/in/silive/bytepad/Ui/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package in.silive.bytepad;
+package in.silive.bytepad.Ui.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import in.silive.bytepad.R;
 
 public class MainActivity extends AppCompatActivity {
 
